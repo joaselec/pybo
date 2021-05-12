6b0de91f97f33a174e081ff153caf2813364044e
joaselec <ki8107@naver.com>
2021-05-13 00:12:55 +0900

    first commit

---
+++ __init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    from views import main_views
+    app.register_blueprint(main_views.bp)
+
+    return app
+
+if __name__ == '__main__':
+    create_app().run()
+
+
